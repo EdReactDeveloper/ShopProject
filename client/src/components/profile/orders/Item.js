a31af20d6332ac51f571039fd3c40ad7fef447be
EdReactDeveloper <edinbox30@gmail.com>
2019-07-21 17:25:41 +0300

    css final

--- client/src/components/profile/orders/Item.js
+++ client/src/components/profile/orders/Item.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const Item = ({product}) => {
-  const {title, color, size, price, quantity, total} = product
-  return (
-    <li className="orders__order_item">
-      <h3>{title}</h3>
-      <div>size: {size}</div>
-      <div style={{background: color}} className="orders__order_item-color"></div>
-      <div>quantity: {quantity}</div>
-      <div>total: ${total}</div>
-      <div>price: ${price}</div>
-    </li>
-  )
-}
+const Item = ({ product }) => {
+	const { title, color, size, price, quantity, total } = product;
+	return (
+		<li className="orders__order_item">
+			<h3 className="orders__order_item_heading">{title}</h3>
+				<div className="orders__order_item_info">
+					<div>size: </div>
+					<div>{size}</div>
+					<div>color: </div>
+					<div style={{ background: color }} className="orders__order_item-color" />
+				</div>
+				<div className="orders__order_item_info">
+					<div>quantity:</div>
+					<div>{quantity}</div>
+					<div>price: </div>
+					<div>${price}</div>
+				</div>
+			<div>total: ${total}</div>
+		</li>
+	);
+};
 
-export default Item
+export default Item;
