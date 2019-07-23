6327875132e459e40c33a524770be5b812e2a6df
EdReactDeveloper <edinbox30@gmail.com>
2019-07-23 13:58:03 +0300

    build

---
+++ client/build/precache-manifest.66ed36986c74e5f72c31db78a978c922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72be5c7f984840577cda547e9a1feed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c5544ef8ba8590d14d3",
+    "url": "/static/css/main.1d0ae1ba.chunk.css"
+  },
+  {
+    "revision": "e7468de3232bc49cd801",
+    "url": "/static/js/2.81315478.chunk.js"
+  },
+  {
+    "revision": "2c5544ef8ba8590d14d3",
+    "url": "/static/js/main.b44f653c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "053cbe39a7da77789121bf7ebdd43346",
+    "url": "/static/media/Bg.053cbe39.png"
+  }
+]);
